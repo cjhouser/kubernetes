665b39ba066ebbacbbc6346fd41af9561e0f83cb
Charles Houser <cjohouser@gmail.com>
2021-10-07 17:43:08 -0700

    counter server

---
+++ counter/counter.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	counter := 0
+	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		counter++
+		fmt.Fprintf(w, "Count: %d", counter)
+	})
+	http.ListenAndServe(":8080", nil)
+}
---
+++ counter/go.mod
@@ -0,0 +1,3 @@
+module github.com/CJHouser/counter
+
+go 1.16
